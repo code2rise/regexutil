a9ea14db302e3dfdd783630978d2f029930ffcfa
cr.rupesh@gmail.com <cr.rupesh@gmail.com@a0972f6a-2b61-ddd5-e7f1-e7a378fedb58>
2013-07-05 04:28:20 +0000

    Committing comments

--- RegexUtil/src/com/rc/regex/RegexUtil.java
+++ RegexUtil/src/com/rc/regex/RegexUtil.java
@@ -296,7 +296,7 @@ public class RegexUtil
 //	{
 //		RegexUtil instance = new RegexUtil();
 		
-//		String email = "cr.rupesh007@g.com";
+//		String email = "alex.bach@g.com";
 //		
 //		System.out.println("Email valid : " + instance.isValidEmail(email));
 //		
